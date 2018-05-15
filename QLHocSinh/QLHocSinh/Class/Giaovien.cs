488e9fc90bb386cb335f0cdeac5aa2a32f376ee2
gioi96 <phamgioi1202@gmail.com>
2018-05-15 15:24:53 +0700

    GV

---
+++ QLHocSinh/QLHocSinh/Class/Giaovien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_Ly_Hoc_Sinh.Class
+{
+    class Giaovien
+    {
+        public int maGV;
+        public String hotenGV;
+        public String maHT;
+        public DateTime ngaysinhGV;
+        public bool gioitinhGV;
+        public String diachiGV;
+        public String soCMNDGV;
+        public String dienthoaiGV;
+        public String trinhdoGV;
+        public String chuyenmonGV;
+        public String chucvuGV;
+        public int maMH;
+        public int LopQuanLy;
+    }
+}
