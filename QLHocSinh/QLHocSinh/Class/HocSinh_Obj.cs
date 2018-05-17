937748b44eb3e42b54df04f6d02b2305a845db97
gioi96 <phamgioi1202@gmail.com>
2018-05-17 15:25:26 +0700

    .

---
+++ QLHocSinh/QLHocSinh/Class/HocSinh_Obj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace Quan_Ly_Hoc_Sinh.Class
+{
+    class HocSinh_Obj
+    {
+        public int MaHS;
+        public string MaHienThi;
+        public string HoTen;
+        public DateTime NgaySinh;
+        public bool GioiTinh;
+        public string DiaChi;
+        public string QueQuan;
+        public string DanToc;
+        public string DienThoai;
+        public string Lop;
+        public string HotenCha;
+        public string NgheNghiepCha;
+        public string DienthoaiCha;
+        public string HotenMe;
+        public string NgheNghiepMe;
+        public string DienthoaiMe;
+    }
+}
