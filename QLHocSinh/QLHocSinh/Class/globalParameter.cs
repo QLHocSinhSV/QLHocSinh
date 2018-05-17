9b2daf5272601cdbd7c176da1f991b29cf3bb274
gioi96 <phamgioi1202@gmail.com>
2018-05-17 15:25:20 +0700

    .

---
+++ QLHocSinh/QLHocSinh/Class/globalParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_Ly_Hoc_Sinh.Class
+{
+    class globalParameter
+    {
+        public static String str = @"Data Source=LENOVO\GIOI;Initial Catalog=QuanLy_GiaoVien_HocSinh_THPT;Integrated Security=True";
+    }
+}
